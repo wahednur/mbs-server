f0b4a7a5ebd7ca80c0bb5eec7504eb4cf9a8a8b9
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-15 03:00:51 +0600

    empty email login block

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+const cookieParsar = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const morgan = require("morgan");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const secrete = process.env.JWT_SECRETE;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // Declare the app
 const app = express();
 
 //Cors options
 const corsOption = {
-  origin: ["http://localhost:5173", "http://localhost:5174", "*"],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -17,13 +22,12 @@ const corsOption = {
 // Middleware
 app.use(cors(corsOption));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParsar());
 app.use(morgan("dev"));
 
 const dbUser = process.env.MDB_USER;
 const dbPass = process.env.MDB_PASS;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.w9uu7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,13 +48,6 @@ async function run() {
     //Database collections
     const userCollection = client.db("bms").collection("users");
 
-    //Jwt token
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, secrete, { expiresIn: "1h" });
-      res.send({ token });
-    });
-
     //Verify token middleware
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
@@ -65,6 +62,27 @@ async function run() {
         next();
       });
     };
+
+    //Jwt token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, secrete, { expiresIn: "1h" });
+      res.send({ token });
+    });
+    //Ceate User
+    app.post("/users", async (req, res) => {
+      const { email } = req.body;
+      const user = req.body;
+      const query = { email: email };
+      const newUser = { ...user, role: "user" };
+      if (!email) return res.send({ message: "You email missing" });
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist);
+      }
+      const restult = await userCollection.insertOne(newUser);
+      res.send(restult);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
